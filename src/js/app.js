acdeee95c19eec6313062f81ed86d546eb7c57a3
Johan Gustafson <johan.gustafson@gmail.com>
2018-01-01 23:59:49 +1300

    Test other hexagon function

--- src/js/app.js
+++ src/js/app.js
@@ -12,25 +12,33 @@ canvas.height = document.body.clientHeight;
 
 mainContent.appendChild(canvas);
 
-//var ctx = canvas.getContext('2d');
-//ctx.fillStyle = 'blue';
-//ctx.fillRect(20, 20, 40, 40);
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'blue';
 
 var hexagonBoard = new Hexagons();
 
-hexagonBoard.draw(canvas);
+//hexagonBoard.draw(canvas);
 
 
+drawHexagon(ctx, 300, 300);
 
-function drawHexagon(ctx, x, y) {
+ctx.fillRect(300, 300, 40, 40);
+
+
+function drawHexagon(context, x, y) {
     var size = 200;
-    ctx.beginPath();
-    ctx.moveTo(x, y - size * 2/3);
-    ctx.lineTo(x + size * 1/2, y - size * 1/4);
-    ctx.lineTo(x + size * 1/2, y + size * 1/4);
-    ctx.lineTo(x, y + size * 2/3);
-    ctx.lineTo(x - size * 1/2, y + size * 1/4);
-    ctx.lineTo(x - size * 1/2, y - size * 1/4);
 
-    ctx.fill();
+    context.save();
+    context.rotate(Math.PI / 6);
+    context.translate(size, -size);
+    context.beginPath();
+    context.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
+
+    for (var side = 0; side < 7; side++) {
+        context.lineTo(x + size * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6));
+    }
+
+    context.fillStyle = "red";
+    context.fill();
+    context.restore();
 }
