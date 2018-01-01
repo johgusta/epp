db78c5d8e293ce414e7d489b2e3f3bb3c8bfc7f9
Johan Gustafson <johan.gustafson@gmail.com>
2018-01-01 22:31:36 +1300

    Remove nzftw game stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,8 @@
 require('./main.css');
-var Game = require('./game.js');
 
 var mainContent = document.getElementById('main-content');
-var gameContainer = document.createElement('div');
-gameContainer.classList.add('game-container');
-mainContent.appendChild(gameContainer);
 
-var game = new Game(gameContainer);
+var span = document.createElement('span');
+span.innerText = 'Hello world';
+mainContent.appendChild(span);
+
--- src/js/montyPuzzle/montyPuzzle.js
+++
@@ -1,9 +0,0 @@
-var createGame = require('./createGame.js');
-var preloadGame = require('./preloadGame.js');
-var updateGame = require('./updateGame.js');
-
-module.exports = {
-    preload: preloadGame,
-    create: createGame,
-    update: updateGame
-};
