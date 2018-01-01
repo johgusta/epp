d775923bff1f0e6d0c055548b6bd782d395cb20e
Johan Gustafson <johan.gustafson@gmail.com>
2018-01-02 07:41:42 +1300

    Remake of hexagon, still have size confusion

--- src/js/app.js
+++ src/js/app.js
@@ -13,16 +13,17 @@ canvas.height = document.body.clientHeight;
 mainContent.appendChild(canvas);
 
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'blue';
 
 var hexagonBoard = new Hexagons();
 
 //hexagonBoard.draw(canvas);
 
 
-drawHexagon(ctx, 300, 300);
+ctx.fillStyle = 'blue';
+ctx.fillRect(100, 100, 200, 200);
+//drawHexagon(ctx, 300, 300);
+breakfastHexagon(ctx, 100, 100);
 
-ctx.fillRect(300, 300, 40, 40);
 
 
 function drawHexagon(context, x, y) {
@@ -41,4 +42,22 @@ function drawHexagon(context, x, y) {
     context.fillStyle = "red";
     context.fill();
     context.restore();
+}
+
+function breakfastHexagon(context, x, y) {
+    var size = 200;
+
+    var topHeight = Math.tan(Math.PI / 6) * size / 2;
+    var hypotenuse = (size / 2) / Math.cos(Math.PI / 6);
+
+    context.beginPath();
+    context.moveTo(x + size / 2, y);
+    context.lineTo(x + size, y + topHeight);
+    context.lineTo(x + size, y + topHeight + hypotenuse);
+    context.lineTo(x + size / 2, y + topHeight * 2 + hypotenuse);
+    context. lineTo(x, y + topHeight + hypotenuse);
+    context.lineTo(x, y + topHeight);
+
+    context.fillStyle = 'green';
+    context.fill();
 }
