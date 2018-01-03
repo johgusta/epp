054b11e61c10ed4e35373483d4a544da17a42532
Johan Gustafson <johan.gustafson@gmail.com>
2018-01-04 09:50:05 +1300

    Add backing map for board

--- src/js/hexagonBoard.js
+++ src/js/hexagonBoard.js
@@ -2,25 +2,73 @@ function HexagonBoard(canvas, size) {
     this.canvas = canvas;
     this.size = size;
 
+    this._board = [];
+
+    this._boardSize = calculateBoardSize(this.canvas.width, this.canvas.height, size);
+
+    var row = [];
+    row[2] = {
+        color: 'blue'
+    };
+
+    this._board[3] = row;
+
     var that = this;
 
     function actualResizeHandler() {
 
         that.canvas.width = document.body.clientWidth - 2;
         that.canvas.height = document.body.clientHeight - 2;
+
+        that._boardSize = calculateBoardSize(that.canvas.width, that.canvas.height, that.size);
         that.draw();
     }
 
     window.addEventListener("resize", _.throttle(actualResizeHandler, 66), false);
+
+    function mouseMoveHandler(event) {
+        console.log('mouse moved', event);
+    }
+
+    this.canvas.addEventListener('mousemove', _.throttle(mouseMoveHandler, 20));
 }
 
 HexagonBoard.prototype.draw = function draw() {
     var ctx = this.canvas.getContext('2d');
-    drawBoard(ctx, this.canvas.width, this.canvas.height, this.size);
+    drawBoard(ctx, this._board, this._boardSize.width, this._boardSize.height, this.size);
 };
 
 
-function drawBoard(ctx, width, height, size) {
+function drawBoard(ctx, board, boardWidth, boardHeight, hexagonSize) {
+
+    var b = (hexagonSize / 2) / Math.cos(Math.PI / 6);
+    var a = Math.sin(Math.PI / 6) * b;
+
+    var hexagonHeight = (2 * a + b);
+
+    var row;
+    var hexagon;
+    var color;
+    var borderColor;
+    var xOffset = 0;
+    for (var i = 0; i < boardHeight; i++) {
+
+        if (i % 2 !== 0) {
+            xOffset = hexagonSize /2;
+        } else {
+            xOffset = 0;
+        }
+
+        row = board[i];
+
+        for (var j = 0; j < boardWidth; j++) {
+            hexagon = row !== undefined ? row[j] : undefined;
+            breakfastHexagon(ctx, hexagon, j * hexagonSize  + xOffset, i * (hexagonHeight - a), hexagonSize);
+        }
+    }
+}
+
+function calculateBoardSize(width, height, size) {
 
     var b = (size / 2) / Math.cos(Math.PI / 6);
     var a = Math.sin(Math.PI / 6) * b;
@@ -31,23 +79,15 @@ function drawBoard(ctx, width, height, size) {
     var boardWidth = Math.floor((width - size / 2) / size);
     var boardHeight = Math.floor((height - a) / (hexagonHeight - a));
 
-
-    var xOffset = 0;
-    for (var i = 0; i < boardHeight; i++) {
-
-        if (i % 2 !== 0) {
-            xOffset = size /2;
-        } else {
-            xOffset = 0;
-        }
-
-        for (var j = 0; j < boardWidth; j++) {
-            breakfastHexagon(ctx, j * size  + xOffset, i * (hexagonHeight - a), size);
-        }
-    }
+    return {
+        width: boardWidth,
+        height: boardHeight
+    };
 }
 
-function breakfastHexagon(context, x, y, size) {
+function breakfastHexagon(context, hexagon, x, y, size) {
+    var color = hexagon && hexagon.color !== undefined ? hexagon.color : 'green';
+    var borderColor = hexagon && hexagon.borderColor !== undefined ? hexagon.borderColor : 'black';
 
     var topHeight = Math.tan(Math.PI / 6) * size / 2;
     var hypotenuse = (size / 2) / Math.cos(Math.PI / 6);
@@ -61,10 +101,10 @@ function breakfastHexagon(context, x, y, size) {
     context.lineTo(x, y + topHeight);
     context.lineTo(x + size / 2, y);
 
-    context.fillStyle = 'green';
+    context.fillStyle = color;
     context.fill();
 
-    context.strokeStyle = 'black';
+    context.strokeStyle = borderColor;
     context.stroke();
 }
 
